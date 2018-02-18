b8c292a4d6fd65329e62f17f5bc6d64a1f7ea0ef
Shadowigor <shadowigorr@gmail.com>
2018-02-18 15:09:50 +0100

    Initial commit

---
+++ history_storage.py
@@ -0,0 +1,76 @@
+
+import zmq
+import psycopg2 as pg
+from common.config import Config
+from common.activity_history import ActivityHistory, InvalidActivityException
+from load_balancer import LoadBalancer
+
+
+class HistoryStorage:
+
+    configs = [
+        {
+            "long": "--server",
+            "short": "-s",
+            "action": "store",
+            "type": str,
+            "help": "Hostname of the PostgreSQL server."
+        },
+        {
+            "long": "--port",
+            "short": "-p",
+            "action": "store",
+            "type": int,
+            "help": "Port of the PostgreSQL server."
+        },
+        {
+            "long": "--user",
+            "short": "-u",
+            "action": "store",
+            "type": str,
+            "help": "Username of the PostgreSQL user."
+        },
+        {
+            "long": "--name",
+            "short": "-n",
+            "action": "store",
+            "type": str,
+            "help": "Name of the PostgreSQL database."
+        },
+        {
+            "long": "--password",
+            "short": "-w",
+            "action": "store",
+            "type": str,
+            "help": "Password of the PostgreSQL database."
+        }
+    ]
+
+    def __init__(self):
+        self.config = Config("Yeli History Storage",
+                             "Stores the activity history for the yeli bot.",
+                             self.configs)
+
+        self.history = ActivityHistory(host = self.config.server,
+                                       port = self.config.port,
+                                       dbname = self.config.name,
+                                       user = self.config.user,
+                                       password = self.config.password)
+        self.zmq_context = zmq.Context.instance()
+        self.zmq_in = self.zmq_context.socket(zmq.PULL)
+        self.balancer = LoadBalancer()
+
+    # TODO Is that the destructor?
+    def __del__(self):
+        self.zmq_context.destroy()
+
+    def poll(self):
+        self.zmq_in.bind("tcp://localhost:8001")
+        try:
+            while True:
+                activity = self.zmq_in.recv_json()
+                if self.history.is_activity_new(activity):
+                    self.history.insert_activity(activity)
+                    self.balancer.new_activity(activity)
+        except KeyboardInterrupt:
+            pass
